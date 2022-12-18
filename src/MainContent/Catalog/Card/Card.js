01dc4a6a535003933748d0fe4efef5ef4f6d1d7c
Viktoryia Zhybul <victoria.zhibul@gmail.com>
2022-12-18 10:34:39 +0300

    check

--- src/MainContent/Catalog/Card/Card.js
+++ src/MainContent/Catalog/Card/Card.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useAuth } from "./../../../Header/use-auth"
-import { addUserFavorites, deliteFavorites } from "./../../../redux/favoritesSlice";
+import { addUserFavorites } from "./../../../redux/favoritesSlice";
 
 import "./Card.css";
 
 
 function Card({info}) {
     const dispatch = useDispatch();
-    const favorites = useSelector( state => state.favorites.idBreed );
     
     const {isAuth, id} = useAuth();
 
-    const addFavorities = (value, idCard) => {
-        if (value === "Добавить в избранное") {
-            dispatch(addUserFavorites(idCard));
+    const addFavorities = (idCard) => {
+        dispatch(addUserFavorites(idCard));
+        alert("Порода успешно добавлена!");
         }
-        if (value === "Удалить из избранного") {
-            dispatch(deliteFavorites(idCard));
-        }
-        }
-
-        function getValue(obj, id) {
-            let value="Добавить в избранное";
-            if ( favorites.includes(id) )
-            obj.value="Удалить из избранного";
-            return value;
-          }
 
     return (
         <div className='card_wrap'>
@@ -38,9 +26,7 @@ function Card({info}) {
             <p className="card__discription">{info.group}</p>
             <p className="card__discription">{info.fur}</p>
             <p className="card__discription">{info.size}</p>
-            {(isAuth)&&(<input type="button" value={"Добавить в избранное"} className="card__button" onClick={(event) =>{
-                getValue(event.target, info.id);
-                addFavorities(event.target.value, info.id)}
+            {(isAuth)&&(<input type="button" value="Добавить в избранное" className="card__button" onClick={() =>{addFavorities( info.id)}
             } />)}
         </div>
     )
